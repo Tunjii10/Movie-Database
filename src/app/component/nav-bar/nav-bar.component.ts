4e6181d298a629c678bf6fbb852d7ce3da3ed77b
Tunjii10 <oluwatunji7@gmail.com>
2022-06-17 00:11:53 +0100

    feat: api service

--- src/app/component/nav-bar/nav-bar.component.ts
+++ src/app/component/nav-bar/nav-bar.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 import { NavSearchInterface } from './nav-search-interface';
-
 @Component({
     selector: 'app-nav-bar',
     templateUrl: './nav-bar.component.html',
